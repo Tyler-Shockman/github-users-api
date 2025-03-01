c445e12ef480ee8e796f6adb90398bc301c4881b
Tyler James Shockman <57335504+Tyler-Shockman@users.noreply.github.com>
2025-02-28 22:18:59 -0600

    Added a simple in memory rate limiter to protect against any kind of DDOS attack.

--- src/main/java/org/branch/github_users_api/controllers/GitHubUsersController.java
+++ src/main/java/org/branch/github_users_api/controllers/GitHubUsersController.java
@@ -1,15 +1,15 @@
 package org.branch.github_users_api.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.branch.github_users_api.domain.dtos.GitHubUserDTO;
 import org.branch.github_users_api.domain.entities.GitHubUser;
 import org.branch.github_users_api.mappers.GitHubUserMapper;
 import org.branch.github_users_api.services.GitHubUsersService;
+import org.branch.github_users_api.services.RateLimiter;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -20,9 +20,11 @@ public final class GitHubUsersController {
 
     private final GitHubUsersService gitHubUsersService;
     private final GitHubUserMapper gitHubUserMapper;
+    private final RateLimiter rateLimiter;
 
     @GetMapping("/{username}")
-    public ResponseEntity<GitHubUserDTO> getGitHubUser(@PathVariable String username) {
+    public ResponseEntity<GitHubUserDTO> getGitHubUser(@PathVariable String username, HttpServletRequest request) {
+        if (!rateLimiter.acquirePermit("getGitHubUsers-" + request.getRemoteAddr())) return new ResponseEntity<>(HttpStatus.TOO_MANY_REQUESTS);
         Optional<GitHubUser> foundGithubUser = gitHubUsersService.findByUsername(username);
 
         return foundGithubUser.map(gitHubUser ->
---
+++ src/main/java/org/branch/github_users_api/services/RateLimiter.java
@@ -0,0 +1,33 @@
+package org.branch.github_users_api.services;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class RateLimiter {
+    private final Map<String, Bucket> buckets = new HashMap<>();
+    private final Bandwidth bandwidth;
+
+    public RateLimiter(
+            @Value("${rate-limit.limit:60}")
+            final int limit,
+            @Value("${rate-limit.limit-seconds:60}")
+            final int limitSeconds
+            ) {
+        this.bandwidth = Bandwidth.classic(limit, Refill.intervally(limit, Duration.ofSeconds(limitSeconds)));
+    }
+
+    public boolean acquirePermit(String token) {
+        if (!buckets.containsKey(token)) {
+            this.buckets.put(token, Bucket.builder().addLimit(this.bandwidth).build());
+        }
+        return this.buckets.get(token).tryConsume(1);
+    }
+}
