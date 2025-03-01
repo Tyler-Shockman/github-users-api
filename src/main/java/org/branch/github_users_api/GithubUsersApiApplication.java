53bd3485b90b93dfd8cdf8326529b12b36e58dc7
Tyler James Shockman <57335504+Tyler-Shockman@users.noreply.github.com>
2025-02-28 18:19:22 -0600

    Initial commit

---
+++ src/main/java/org/branch/github_users_api/GithubUsersApiApplication.java
@@ -0,0 +1,13 @@
+package org.branch.github_users_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubUsersApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubUsersApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/branch/github_users_api/GithubUsersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.branch.github_users_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubUsersApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
